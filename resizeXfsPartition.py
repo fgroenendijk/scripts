81beae7731ed512d22425c05b531522625be6182
Floris Groenendijk <weirdall@gmail.com>
2017-06-02 11:36:12 +0200

    resize partition using -s (script)

--- resizeXfsPartition.py
+++ resizeXfsPartition.py
@@ -1,24 +1,47 @@
+#!/usr/bin/env python
 import re
+import getopt
 import subprocess
+import sys
 
-try:
-        partition = subprocess.check_output(["parted", "/dev/xvdf", "print"], stderr=subprocess.STDOUT)
+def main( argv = None ):
+        if argv == None:
+		print 'resizeXfsPartition.py /dev/sda'
+		sys.exit()
+	try:
+		opts, args = getopt.getopt(argv,"h:")
+	except getopt.GetoptError:
+		print 'resizeXfsPartition.py /dev/sda'
+		sys.exit(2)
 
-        startOfPartition = None
-        for line in partition.split( '\n' ):
-                if re.match( '^ 1', line ):
-                        row = re.split( ' +', line.strip() )
-                        if len( row ) > 1:
-                                startOfPartition = row[1]
-                                print startOfPartition
+	for opt, arg in opts:
+		if opt == '-h':
+			print 'resizeXfsPartition.py /dev/sda'
+			sys.exit()
+	for arg in args:
+		diskname = arg
 
-        if startOfPartition != None:
-                partition = subprocess.check_output(["parted", "/dev/xvdf", "rm", "1"], stderr=subprocess.STDOUT)
-                print partition
-                partition = subprocess.check_output(["parted", "/dev/xvdf", "mkpart", "primary", "ext2", startOfPartition, "-1"], stderr=subprocess.STDOUT)
-                print partition
-                partition = subprocess.check_output(["parted", "/dev/xvdf", "print"], stderr=subprocess.STDOUT)
-                print partition
+	try:
+	        partition = subprocess.check_output(["parted", "-s", diskname, "print"], stderr=subprocess.STDOUT)
 
-except subprocess.CalledProcessError as e:
-        print e.output
+		startOfPartition = None
+		for line in partition.split( '\n' ):
+			if re.match( '^ 1', line ):
+				row = re.split( ' +', line.strip() )
+				if len( row ) > 1:
+					startOfPartition = row[1]
+					print startOfPartition
+
+		if startOfPartition != None:
+			partition = subprocess.check_output(["parted", "-s", diskname, "rm", "1"], stderr=subprocess.STDOUT)
+			print partition
+			partition = subprocess.check_output(["parted", "-s", diskname, "mkpart", "primary", "ext2", startOfPartition, "-1"], stderr=subprocess.STDOUT)
+			print partition
+			partition = subprocess.check_output(["parted", "-s", diskname, "print"], stderr=subprocess.STDOUT)
+			print partition
+
+	except subprocess.CalledProcessError as e:
+	        print e.output
+
+if __name__ == "__main__":
+	main(sys.argv[1:])
